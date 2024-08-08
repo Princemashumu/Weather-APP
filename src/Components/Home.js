e4e0d43d18a3602c489a4c36659e7c0c599c1029
Princemashumu <113059693+Princemashumu@users.noreply.github.com>
2024-08-09 00:19:04 +0200

    Privacy and security

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import BgImg from '../assets/BgImg.jpg'; // Adjust the path based on your project structure
-import { Typography, Container, Box, Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions,Snackbar,Alert } from '@mui/material';
+import { Typography, Container, Box, Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions, Snackbar, Alert, Checkbox, FormControlLabel, Link } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
-
 
 const Home = () => {
   const navigate = useNavigate();
   const [openRegister, setOpenRegister] = useState(false);
   const [openLogin, setOpenLogin] = useState(false);
+  const [openPrivacyPolicy, setOpenPrivacyPolicy] = useState(false); // State for privacy policy dialog
+  const [openTermsConditions, setOpenTermsConditions] = useState(false); // State for terms & conditions dialog
   const [registerData, setRegisterData] = useState({ username: '', email: '', password: '', confirmPassword: '' });
   const [loginData, setLoginData] = useState({ email: '', password: '' });
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarSeverity, setSnackbarSeverity] = useState('error');
+  const [termsAccepted, setTermsAccepted] = useState(false);
 
   const handleOpenRegister = () => setOpenRegister(true);
   const handleCloseRegister = () => setOpenRegister(false);
 
   const handleOpenLogin = () => setOpenLogin(true);
   const handleCloseLogin = () => setOpenLogin(false);
+
+  const handleOpenPrivacyPolicy = () => setOpenPrivacyPolicy(true);
+  const handleClosePrivacyPolicy = () => setOpenPrivacyPolicy(false);
+
+  const handleOpenTermsConditions = () => setOpenTermsConditions(true);
+  const handleCloseTermsConditions = () => setOpenTermsConditions(false);
 
   const handleRegisterChange = (e) => {
     const { name, value } = e.target;
@@ -30,6 +37,10 @@ const Home = () => {
   const handleLoginChange = (e) => {
     const { name, value } = e.target;
     setLoginData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleTermsChange = (e) => {
+    setTermsAccepted(e.target.checked);
   };
 
   const handleSnackbarClose = () => setSnackbarOpen(false);
@@ -43,6 +54,12 @@ const Home = () => {
 
     if (registerData.password !== registerData.confirmPassword) {
       setSnackbarMessage('Passwords do not match');
+      setSnackbarOpen(true);
+      return;
+    }
+
+    if (!termsAccepted) {
+      setSnackbarMessage('You must accept the terms & conditions and privacy policy');
       setSnackbarOpen(true);
       return;
     }
@@ -98,7 +115,6 @@ const Home = () => {
     }
   };
 
-
   return (
     <Box
       sx={{
@@ -107,7 +123,7 @@ const Home = () => {
         alignItems: 'baseline',
         flexDirection: 'column',
         height: '100vh',
-        maxWidth:'100%',
+        maxWidth: '100%',
         backgroundImage: `url(${BgImg})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
@@ -116,9 +132,8 @@ const Home = () => {
         color: 'white',
         padding: '20px',
         boxSizing: 'border-box',
-        // msOverflowY:'hidden',
-        overflow:'hidden',
-        borderRadius:'25px',
+        overflow: 'hidden',
+        borderRadius: '25px',
       }}
     >
       <Container maxWidth="md">
@@ -132,24 +147,43 @@ const Home = () => {
           This app is designed to provide users with an intuitive and interactive weather experience. Explore the features and enjoy the seamless functionality offered by our platform.
         </Typography>
         <Box sx={{ mt: 4, display: 'flex', justifyContent: 'center', gap: 2, flexWrap: 'wrap' }}>
-        <Button variant="contained" color="primary" onClick={handleOpenRegister}>
+          <Button variant="contained" color="primary" onClick={handleOpenRegister}>
             Register Account
           </Button>
           <Button variant="contained" color="secondary" onClick={handleOpenLogin}>
-          Login
+            Login
           </Button>
         </Box>
       </Container>
 
-
-
-      <Dialog open={openRegister} onClose={handleCloseRegister} PaperProps={{ sx: { backgroundColor: 'rgba(155, 155, 155, 0)', backdropFilter: 'blur(10px)', borderRadius:'25px', boxShadow:'50px',color:'white'} }}>
+      <Dialog open={openRegister} onClose={handleCloseRegister} PaperProps={{ sx: { backgroundColor: 'rgba(155, 155, 155, 0)', backdropFilter: 'blur(10px)', borderRadius: '25px', boxShadow: '50px', color: 'white' } }}>
         <DialogTitle>Create Account</DialogTitle>
         <DialogContent>
           <TextField autoFocus margin="dense" name="username" label="Username" type="text" fullWidth variant="outlined" value={registerData.username} onChange={handleRegisterChange} />
           <TextField margin="dense" name="email" label="Email Address" type="email" fullWidth variant="outlined" value={registerData.email} onChange={handleRegisterChange} />
           <TextField margin="dense" name="password" label="Password" type="password" fullWidth variant="outlined" value={registerData.password} onChange={handleRegisterChange} />
           <TextField margin="dense" name="confirmPassword" label="Confirm Password" type="password" fullWidth variant="outlined" value={registerData.confirmPassword} onChange={handleRegisterChange} />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={termsAccepted}
+                onChange={handleTermsChange}
+                color="primary"
+              />
+            }
+            label={
+              <Typography component="span">
+                I agree to the{' '}
+                <Link component="button" onClick={handleOpenPrivacyPolicy} color="inherit">
+                  Privacy Policy
+                </Link>{' '}
+                and{' '}
+                <Link component="button" onClick={handleOpenTermsConditions} color="inherit">
+                  Terms & Conditions
+                </Link>
+              </Typography>
+            }
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseRegister} color="primary">Cancel</Button>
@@ -157,7 +191,31 @@ const Home = () => {
         </DialogActions>
       </Dialog>
 
-      <Dialog open={openLogin} onClose={handleCloseLogin} PaperProps={{sx: { backgroundColor: 'rgba(155, 155, 155, 0)', backdropFilter: 'blur(10px)', borderRadius:'25px', boxShadow:'50px',color:'white'}}}>
+      <Dialog open={openPrivacyPolicy} onClose={handleClosePrivacyPolicy} PaperProps={{ sx: { backgroundColor: 'rgba(155, 155, 155, 0)', backdropFilter: 'blur(10px)', borderRadius: '25px', boxShadow: '50px', color: 'white' } }}>
+        <DialogTitle>Privacy Policy</DialogTitle>
+        <DialogContent>
+          <Typography variant="body1" component="p">
+            This Privacy Policy explains how we handle your personal information and complies with the Protection of Personal Information Act (POPI Act). We collect location data to provide you with personalized weather forecasts. Your data is stored securely and is only used for the intended purpose of improving your experience on our platform.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClosePrivacyPolicy} color="primary">Close</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openTermsConditions} onClose={handleCloseTermsConditions} PaperProps={{ sx: { backgroundColor: 'rgba(155, 155, 155, 0)', backdropFilter: 'blur(10px)', borderRadius: '25px', boxShadow: '50px', color: 'white' } }}>
+        <DialogTitle>Terms & Conditions</DialogTitle>
+        <DialogContent>
+          <Typography variant="body1" component="p">
+            By using this app, you agree to our Terms & Conditions. The use of our app is subject to these terms, which include your responsibilities and the limitations of our liability. Please read these terms carefully. If you do not agree with any part of these terms, do not use the app.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseTermsConditions} color="primary">Close</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openLogin} onClose={handleCloseLogin} PaperProps={{ sx: { backgroundColor: 'rgba(155, 155, 155, 0)', backdropFilter: 'blur(10px)', borderRadius: '25px', boxShadow: '50px', color: 'white' } }}>
         <DialogTitle>Login</DialogTitle>
         <DialogContent>
           <TextField autoFocus margin="dense" name="email" label="Email Address" type="email" fullWidth variant="outlined" value={loginData.email} onChange={handleLoginChange} />
@@ -168,7 +226,6 @@ const Home = () => {
           <Button onClick={handleLoginSubmit} color="primary">Login</Button>
         </DialogActions>
       </Dialog>
-
 
       <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
         <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: '100%' }}>
